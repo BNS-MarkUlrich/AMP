d883959a445de231ae24e6faccf68320f5a66223
BNS-MarkUlrich <31822@ma-web.nl>
2020-09-25 11:12:40 +0200

    Added movement animation, key response and removed old images

--- script.js
+++ script.js
@@ -31,9 +31,42 @@ addEventListener('keydown',(e)=>{
       car.speed += 0.5
       break;
     case "ArrowLeft":
-      car.speed -= -.5;
+      car.speed -= 0.5;
       break;
   }
 })
+
+car.image.addEventListener('load',()=>{
+  setInterval(animate,10)
+})
+
+function animate(){
+  context.clearRect(0,0,width,height);
+  context.drawImage(car.image,car.pos,0);
+
+  context.save()
+  context.translate(car.pos + 213,200)
+  context.rotate(wheels.angle);
+  context.drawImage(wheels.backWheel,-wheels.backWheel.width/2,-wheels.backWheel.height/2)
+  context.restore()
+
+  context.save()
+  context.translate(car.pos + 753,200)
+  context.rotate(wheels.angle);
+  context.drawImage(wheels.frontWheel,-wheels.frontWheel.width/2,-wheels.frontWheel.height/2)
+  context.restore()
+
+
+  car.pos += car.speed;
+  if(car.pos > width){
+    car.pos = -car.image.width
+  }
+  if(car.pos < -car.image.width){
+    car.pos = width;
+  }
+
+  wheels.angle += car.speed / (0.5*wheels.backWheel.width);
+  //console.log(car.speed / (0.5*wheels.backWheel.width))
+}
 
 // create namespace
