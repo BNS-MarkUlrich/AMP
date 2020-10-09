c6192a3a2207928e7776c85e6beb72eaab69c68b
BNS-MarkUlrich <31822@ma-web.nl>
2020-10-09 12:44:05 +0200

    Hourshand now also takes into account minutes

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function animate(){
 
   context.save()
   context.translate(clockFace.width/2,clockFace.height/2)
-  context.rotate(hours*2*Math.PI/12);
+  context.rotate(hours*2*Math.PI/12+minutes*Math.PI/720);
   context.drawImage(hoursHand,-hoursHand.width/2,-hoursHand.height);
   context.restore()
 
